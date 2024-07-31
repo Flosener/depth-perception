3d69c2f086d2b331a1111b09984c47df7735c5b1
Florian Pätzold <fpaetzold@uni-osnabrueck.de>
2024-07-31 10:33:48 +0200

    added DAv2 and metric depth estimation

--- Project/estimate_metric.py
+++ Project/estimate_metric.py
@@ -1,5 +1,5 @@
 """
-Copyright 2024 LiheYoung
+Copyright 2024 Lihe Yang
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
@@ -14,14 +14,14 @@ Copyright 2024 LiheYoung
    limitations under the License.
 
 Citation:
-@inproceedings{depthanything,
-      title={Depth Anything: Unleashing the Power of Large-Scale Unlabeled Data}, 
-      author={Yang, Lihe and Kang, Bingyi and Huang, Zilong and Xu, Xiaogang and Feng, Jiashi and Zhao, Hengshuang},
-      booktitle={CVPR},
-      year={2024}
+@article{depth_anything_v2,
+  title={Depth Anything V2},
+  author={Yang, Lihe and Kang, Bingyi and Huang, Zilong and Zhao, Zhen and Xu, Xiaogang and Feng, Jiashi and Zhao, Hengshuang},
+  journal={arXiv:2406.09414},
+  year={2024}
 }
 
-GitHub: https://github.com/LiheYoung/Depth-Anything
+GitHub: https://github.com/DepthAnything/Depth-Anything-V2
 """
 
 # region Imports
@@ -31,7 +31,7 @@ from pathlib import Path
 # Use the project file packages instead of the conda packages, i.e. add to system path for import
 file = Path(__file__).resolve()
 root = file.parents[0]
-modules = ['yolov5', 'depthanything']
+modules = ['yolov5', 'depthanythingv2']
 for m in modules:
     path = root / m
     if path.exists() and str(path) not in sys.path:
@@ -42,26 +42,31 @@ for m in modules:
     elif str(path) in sys.path:
         print(f"{path} already exists in sys.path")
 
-import os
-from PIL import Image
-import numpy as np
+
+import argparse
 import cv2
+import glob
+import matplotlib
+import numpy as np
+import os
 import torch
-from torchvision.transforms import Compose
-from depthanything.depth_anything.dpt import DepthAnything
-from depthanything.depth_anything.util.transform import Resize, NormalizeImage, PrepareForNet
+import open3d as o3d
+
+from depthanythingv2.metric_depth.depth_anything_v2.dpt import DepthAnythingV2
 # endregion
 
 class DepthEstimator:
-    def __init__(self, metric_weights, encoder, device=None):
+    def __init__(self, encoder, dataset='hypersim', max_depth=20, device=None):
         self.device = self.get_device(device)
-        self.metric_weights = metric_weights
-        self.encoder = encoder if encoder in ['vits', 'vitb', 'vitl'] else 'vitb'
-        self.config = {
-            'vitl': {'encoder': 'vitl', 'features': 256, 'out_channels': [256, 512, 1024, 1024]},
-            'vitb': {'encoder': 'vitb', 'features': 128, 'out_channels': [96, 192, 384, 768]},
-            'vits': {'encoder': 'vits', 'features': 64, 'out_channels': [48, 96, 192, 384]}
+        self.encoder = encoder if encoder in ['vits', 'vitb', 'vitl', 'vitg'] else 'vitb'
+        self.configs = {
+        'vits': {'encoder': 'vits', 'features': 64, 'out_channels': [48, 96, 192, 384]},
+        'vitb': {'encoder': 'vitb', 'features': 128, 'out_channels': [96, 192, 384, 768]},
+        'vitl': {'encoder': 'vitl', 'features': 256, 'out_channels': [256, 512, 1024, 1024]},
+        'vitg': {'encoder': 'vitg', 'features': 384, 'out_channels': [1536, 1536, 1536, 1536]}
         }
+        self.dataset = dataset
+        self.max_depth = max_depth # 20 for indoor model, 80 for outdoor model
         self.model = self.load_model()
     
     def get_device(self, device):
@@ -73,56 +78,64 @@ class DepthEstimator:
         return torch.device("cpu")
     
     def load_model(self):
-        # online method
-        model = DepthAnything.from_pretrained(f'LiheYoung/depth_anything_{self.encoder}14')
-
-        # offline method (requires weights download!)
-        #model = DepthAnything(self.config[self.encoder])
-        #model.load_state_dict(torch.load(f'./checkpoints/depth_anything_{self.encoder}14.pth'))
-
-        # Load model weights for metric depth (requires weights download!)
-        metric_weights = torch.load(self.metric_weights, map_location=self.device)
-        model.load_state_dict(metric_weights, strict=False)
+        model = DepthAnythingV2(**{**self.configs[self.encoder], 'max_depth': self.max_depth})
+        model.load_state_dict(torch.load(f'depthanythingv2/checkpoints/depth_anything_v2_metric_{self.dataset}_{self.encoder}.pth', map_location=self.device))
         model.to(self.device)
         return model
     
     def preprocess(self, image):
-        h, w = image.shape[:2]
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        transform = Compose([
-            Resize(
-                width=w,
-                height=h,
-                resize_target=False,
-                keep_aspect_ratio=True,
-                ensure_multiple_of=14,
-                resize_method='lower_bound',
-                image_interpolation_method=cv2.INTER_CUBIC,
-            ),
-            NormalizeImage(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-            PrepareForNet(),
-        ])
-        image = transform({'image': image})['image']
-        image = torch.from_numpy(image).unsqueeze(0).to(self.device)
+        # pre-processing already done in infer_image()
         return image
     
     def predict_depth(self, image):
         self.model.eval()
         input = self.preprocess(image)
         with torch.no_grad():
-            depth = self.model(input) # (1,H,W)
+            depth = self.model.infer_image(input, input_size=518)
         return depth
 
     def visualize_depth(self, image, depth, grayscale):
-        h, w = image.shape[:2]
-        depth_image = torch.nn.functional.interpolate(depth[None], (h, w), mode='bilinear', align_corners=False)[0, 0]
-        depth_image = (depth_image - depth_image.min()) / (depth_image.max() - depth_image.min()) * 255.0
-        depth_image = depth_image.cpu().numpy().astype(np.uint8) # (H,W)
-        depth_image = cv2.applyColorMap(depth_image, cv2.COLORMAP_INFERNO) # (H,W,3)
+        h,w = image.shape[:2]
 
-        split_region = np.ones((image.shape[0], 20, 3), dtype=np.uint8) * 255
-        combined_results = cv2.hconcat([image, split_region, depth_image])
-        return combined_results
+        depth_normalized = (depth - depth.min()) / (depth.max() - depth.min()) * 255.0
+        depth_normalized = depth_normalized.astype(np.uint8)
+
+        if grayscale:
+            depth_colored = np.repeat(depth_normalized[..., np.newaxis], 3, axis=-1)
+        else:
+            cmap = matplotlib.colormaps.get_cmap('Spectral_r')
+            depth_colored = (cmap(depth_normalized)[:, :, :3] * 255)[:, :, ::-1].astype(np.uint8)
+
+        if depth_colored is None:
+            return image
+        else:
+            split_region = np.ones((h, 20, 3), dtype=np.uint8) * 255
+            combined_frame = cv2.hconcat([image, split_region, depth_colored])
+            return combined_frame
+        
+    def create_point_cloud(self, image, depth_map, name, outdir):
+        """
+        Code by @ Subhransu Sekhar Bhattacharjee (Rudra) "1ssb"
+        """
+        height, width = image.shape[:2]
+        focal_length_x = 470.4 # adjust according to camera
+        focal_length_y = 470.4 # adjust according to camera
+
+        x, y = np.meshgrid(np.arange(width), np.arange(height))
+        x = (x - width / 2) / focal_length_x
+        y = (y - height / 2) / focal_length_y
+        z = np.array(depth_map)
+
+        points = np.stack((np.multiply(x, z), np.multiply(y, z), z), axis=-1).reshape(-1, 3)
+        colors = image.reshape(-1, 3) / 255.0
+
+        pcd = o3d.geometry.PointCloud()
+        pcd.points = o3d.utility.Vector3dVector(points)
+        pcd.colors = o3d.utility.Vector3dVector(colors)
+
+        out_path = os.path.join(outdir, f'{name}_point_cloud.ply')
+        o3d.io.write_point_cloud(out_path, pcd)
+        print(f'Point cloud saved to {out_path}')
 
 
 def main():
@@ -135,13 +148,15 @@ def main():
 
     # Load DE model
     depth_estimator = DepthEstimator(
-        metric_weights="./checkpoints/depth_anything_metric_depth_indoor.pt",
-        encoder='vits',
-        device='cpu'
+        encoder='vitb',
+        dataset='hypersim', # 'hypersim' for indoor model, 'vkitti' for outdoor model
+        max_depth=20, # 20 for indoor model, 80 for outdoor model
+        device='cpu' # change dpt.py line 220 to use 'cuda'
     )
 
     # Load DS
     ds = os.listdir('./datasets/HaND_augmented/images/')
+    outdir = './datasets/HaND_augmented/visualization/pointclouds/'
     n = len(ds)
     
     # Loop over testing data
@@ -164,6 +179,7 @@ def main():
         # Perform depth estimation
         depth = depth_estimator.predict_depth(frame)
         visual = depth_estimator.visualize_depth(frame, depth, False)
+        #depth_estimator.create_point_cloud(frame, depth, file, outdir)
         print(f'\nDepth Image {i+1}/{n}, Min = {depth.min()}, Max = {depth.max()}')
         cv2.imshow("Depth map", visual)
         if cv2.waitKey(100) & 0xFF == ord('q'):
